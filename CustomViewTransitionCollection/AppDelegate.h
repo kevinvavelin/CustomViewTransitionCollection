584093762b7cf8b9f1d64e50aede9d8a7b8819c9
Vavelin Kevin <vavelinkevin@gmail.com>
2014-04-19 02:57:51 +0400

    Initial Commit

---
+++ CustomViewTransitionCollection/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CustomViewTransitionCollection
+//
+//  Created by Vavelin Kevin on 19/04/14.
+//  Copyright (c) 2014 Vavelin Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CustomViewTransitionCollection/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CustomViewTransitionCollection
+//
+//  Created by Vavelin Kevin on 19/04/14.
+//  Copyright (c) 2014 Vavelin Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
